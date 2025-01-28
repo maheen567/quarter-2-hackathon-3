30b0f413a6ac09f44832fc30ca7264d7755ee6f7
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2025-01-28 03:20:34 -1200

    Add files via upload

---
+++ day4-dynamic-frontend/Data/MenuData.ts
@@ -0,0 +1,66 @@
+interface MenuItem {
+    id: number
+    image: any
+    title: string
+    description: string
+    price: number
+  }
+
+export const menu:MenuItem[] = [
+    {
+        id: 1,
+        image: '/homePhotos/menu_1.jpeg',
+        title: 'Lettuce leaf',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 12.5,
+    },
+    {
+        id: 2,
+        image: '/homePhotos/menu_2.jpeg',
+        title: 'Fresh Breakfast',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 14.5,
+    },
+    {
+        id: 3,
+        image: '/homePhotos/menu_3.jpeg',
+        title: 'Mild Butter',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 12.5,
+    },
+    {
+        id: 4,
+        image: '/homePhotos/menu_4.jpeg',
+        title: 'Fresh Bread',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 12.5,
+    },
+    {
+        id: 5,
+        image: '/homePhotos/menu_5.jpeg',
+        title: 'Glow Cheese',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 12.5,
+    },
+    {
+        id: 6,
+        image: '/homePhotos/menu_6.jpeg',
+        title: 'Italian Pizza',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 12.5,
+    },
+    {
+        id: 7,
+        image: '/homePhotos/menu_7.jpeg',
+        title: 'Slice Beef',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 18.5,
+    },
+    {
+        id: 8,
+        image: '/homePhotos/menu_8.jpeg',
+        title: 'Mushroom Pizza',
+        description: 'Lorem ipsum dolor sit amet.',
+        price: 11.8,
+    },
+]
