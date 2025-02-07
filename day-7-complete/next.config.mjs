598dd5c376a9705ec59e59756246684a0f778650
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2025-02-07 04:55:44 -1200

    Delete day-7-complete directory

--- day-7-complete/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:{
-        domains: ['cdn.sanity.io', 'sanity-nextjs-rouge.vercel.app'],
-    }
-};
-
-export default nextConfig;
--- day-7-complete/sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import chefs from './chefs'
-import foods from './foods'
-import order from './order'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [chefs, foods, order],
-}
