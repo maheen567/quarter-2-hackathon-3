3517a7b1d7c17f6d3e7ea725506e5b6b6d6214ee
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2025-02-07 05:29:55 -1200

    Add files via upload

---
+++ day-7-complete/Data/Chefs.ts
@@ -0,0 +1,33 @@
+interface Chefs {
+    id: number
+    image: any
+    chefName: string
+    position: string
+}
+
+export const chefs:Chefs[] = [
+    {
+        id: 1,
+        image: '/homePhotos/chefs_1.jpeg',
+        chefName: 'D.Estwood',
+        position: 'Chief Chef',
+    },
+    {
+        id: 2,
+        image: '/homePhotos/chefs_2.jpeg',
+        chefName: 'D.Scoriesh',
+        position: 'Assistant Chef',
+    },
+    {
+        id: 3,
+        image: '/homePhotos/chefs_3.jpeg',
+        chefName: 'M. William',
+        position: 'Advertising Chef',
+    },
+    {
+        id: 4,
+        image: '/homePhotos/chefs_4.jpeg',
+        chefName: 'W.Readfroad',
+        position: 'Chef',
+    },
+]
